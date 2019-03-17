f78c9df11b3fa4549c9bd4020b57e07891ae06e9
P.Jeffrey <peterk.jeffrey@sa.gov.au>
2019-03-17 13:38:23 +1030

    Conway's Game of Life final

--- life/src/au/pkj/life/GameState.java
+++ life/src/au/pkj/life/GameState.java
@@ -1,13 +1,18 @@
 package au.pkj.life;
 
+import java.awt.Dimension;
+import java.util.Random;
+
 public class GameState {
 	private boolean[][] _gameBoard;
+	private Random _random;
 	
 	public GameState(int width, int height) {
 		if (width < 3 || height < 3)
 			throw new IllegalArgumentException("Width and Height arguments must be positive integers greater than 2.");
 		
 		_gameBoard = createGameBoard(width, height);
+		_random = new Random(System.currentTimeMillis());
 	}
 	
 	public int getWidth() {
@@ -23,35 +28,41 @@ public class GameState {
 	}
 	
 	public void setup(String seed) {
-		// "3 3 bo$2bo$3o!"
-		String[] parts = seed.split(" ");
-		if (parts.length == 3) {
-			int width = Integer.parseInt(parts[0]);
-			int height = Integer.parseInt(parts[1]);
-			int colOffset = (getWidth() - width) / 2;
-			int rowOffset = (getHeight() - height) / 2;
-			int col = colOffset;
-			int row = rowOffset;
-			int count = 0;
-			for (int i = 0; i < parts[2].length(); i++) {
-				char c = parts[2].charAt(i);
-				if (c == 'b') {
-					col += count == 0 ? 1 : count;
-					count = 0;
-				} else if (c == 'o') {
-					int inc = count == 0 ? 1 : count;
-					while (inc-- > 0)
-						_gameBoard[row][col++] = true;
-					count = 0;
-				} else if (c == '$') {
-					col = colOffset;
-					row += count == 0 ? 1 : count;
-					count = 0;
-				} else if (c == '!') {
-					// nothing, reached end
-				} else {
-					int digit = c - '0';
-					count = count * 10 + digit;
+		if ("random".equals(seed.substring(0, 6))) {
+			int number = Integer.parseInt(seed.substring(6).trim());
+			for (int i = 0; i < number; i++) {
+				int row = weightedRandom(getHeight());
+				int col = weightedRandom(getWidth());
+				_gameBoard[row][col] = true;
+			}
+		} else {
+			Dimension seedSize = calcSeedSize(seed);
+			if (seedSize.width <= getWidth() && seedSize.height <= getHeight()) {
+				int colOffset = (getWidth() - seedSize.width) / 2;
+				int rowOffset = (getHeight() - seedSize.height) / 2;
+				int col = colOffset;
+				int row = rowOffset;
+				int count = 0;
+				for (int i = 0; i < seed.length(); i++) {
+					char c = seed.charAt(i);
+					if (c == 'b') {
+						col += count == 0 ? 1 : count;
+						count = 0;
+					} else if (c == 'o') {
+						int inc = count == 0 ? 1 : count;
+						while (inc-- > 0)
+							_gameBoard[row][col++] = true;
+						count = 0;
+					} else if (c == '$') {
+						col = colOffset;
+						row += count == 0 ? 1 : count;
+						count = 0;
+					} else if (c == '!') {
+						// nothing, reached end
+					} else {
+						int digit = c - '0';
+						count = count * 10 + digit;
+					}
 				}
 			}
 		}
@@ -74,6 +85,33 @@ public class GameState {
 		_gameBoard = newGameBoard;
 	}
 	
+	private Dimension calcSeedSize(String seed) {
+		Dimension size = new Dimension(0, 0);
+		
+		int col = 0;
+		int row = 0;
+		int count = 0;
+		for (int i = 0; i < seed.length(); i++) {
+			char c = seed.charAt(i);
+			if (c == 'b' || c == 'o') {
+				col += count == 0 ? 1 : count;
+				count = 0;
+			} else if (c == '$' || c == '!') {
+				if (col > size.width)
+					size.width = col;
+				col = 0;
+				row += count == 0 ? 1 : count;
+				count = 0;
+			} else {
+				int digit = c - '0';
+				count = count * 10 + digit;
+			}
+		}
+		size.height = row;
+		
+		return size;
+	}
+	
 	private int countLiveNeighbours(int row, int col) {
 		int prevRow = (row == 0 ? getHeight() - 1 : row - 1);
 		int nextRow = (row == getHeight() -1 ? 0 : row + 1);
@@ -92,5 +130,13 @@ public class GameState {
 	
 	private boolean[][] createGameBoard(int width, int height) {
 		return new boolean[height][width];
+	}
+	
+	private int weightedRandom(int max) {
+	    int num = 0;
+	    for (int i = 0; i < 5; i++) {
+	        num += _random.nextInt(max) / 5;
+	    }    
+	    return num;
 	}
 }
--- life/src/au/pkj/life/GameUI.java
+++ life/src/au/pkj/life/GameUI.java
@@ -6,19 +6,31 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JSpinner;
+import javax.swing.JTextField;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
-public class GameUI extends JFrame implements ActionListener, ComponentListener {
+public class GameUI extends JFrame implements ActionListener, ChangeListener {
 	private static final long serialVersionUID = 1L;
+	private static final int DEFAULT_SPEED = 90;
+	private static final int MIN_SPEED = 1;
+	private static final int MAX_SPEED = 99;
 	
+	private int _speed = MAX_SPEED + 1 - DEFAULT_SPEED;
+	private JButton _startStopButton;
+	private JTextField _seedText;
 	private GameBoard _gameBoard;
 	private GameState _gameState;
 	private GameThread _gameThread;
@@ -28,19 +40,24 @@ public class GameUI extends JFrame implements ActionListener, ComponentListener 
 	}
 	
 	public void onSetup() {
-		_gameState = new GameState(_gameBoard.getGridWidth(), _gameBoard.getGridHeight());
-		_gameState.setup("47 5 2o2b2o2b2o2b2o2b2o2b2o2b2o2b2o2b2o2b2o2b2o2b2o$2o2b2o2b2o2b2o2b2o2b2o2b2o2b2o2b2o2b2o2b2obo2bo$44b2o$2o2b2o2b2o2b2o2b2o2b2o2b2o2b2o2b2o2b2o2b2o$2o2b2o2b2o2b2o2b2o2b2o2b2o2b2o2b2o2b2o2b2o!");
+		_gameState = new GameState(_gameBoard.getGameWidth(), _gameBoard.getGameHeight());
+		_gameState.setup(_seedText.getText());
 	}
 	
 	public void onStart() {
 		if (_gameState != null) {
+			_startStopButton.setText("Stop");
+			_startStopButton.setActionCommand("stop");
 			_gameThread = new GameThread(this);
+			_gameThread.setFramePeriod(_speed * 10);
 			_gameThread.setRunning(true);
 			_gameThread.start();
 		}
 	}
 	
 	public void onStop() {
+		_startStopButton.setText("Start");
+		_startStopButton.setActionCommand("start");
 		if (_gameThread != null) {
 			_gameThread.setRunning(false);
 			while (_gameThread != null) {
@@ -67,29 +84,55 @@ public class GameUI extends JFrame implements ActionListener, ComponentListener 
 		_gameBoard = new GameBoard();
 		getContentPane().add(_gameBoard, BorderLayout.CENTER);
 		
-		JPanel buttonsPanel = new JPanel();
-		JButton button;
-		buttonsPanel.setLayout(new BoxLayout(buttonsPanel, BoxLayout.LINE_AXIS));
-		buttonsPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-		buttonsPanel.add(Box.createHorizontalGlue());
-		button = new JButton("Setup");
+		JPanel bottomPanel = new JPanel();
+		bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.LINE_AXIS));
+		bottomPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		
+		ButtonGroup radioGroup = new ButtonGroup();
+		JRadioButton radio = new JRadioButton("Small");
+		radio.setActionCommand("gridSmall");
+		radio.setSelected(true);
+		radio.addActionListener(this);
+		bottomPanel.add(radio);
+		radioGroup.add(radio);
+		radio = new JRadioButton("Medium");
+		radio.setActionCommand("gridMedium");
+		radio.addActionListener(this);
+		bottomPanel.add(radio);
+		radioGroup.add(radio);
+		radio = new JRadioButton("Large");
+		radio.setActionCommand("gridLarge");
+		radio.addActionListener(this);
+		bottomPanel.add(radio);
+		radioGroup.add(radio);
+		bottomPanel.add(Box.createRigidArea(new Dimension(10, 0)));
+		
+		JLabel label = new JLabel("Speed:");
+		bottomPanel.add(label);
+		JSpinner speed = new JSpinner(new SpinnerNumberModel(DEFAULT_SPEED, MIN_SPEED, MAX_SPEED, 1));
+		speed.addChangeListener(this);
+		bottomPanel.add(speed);
+		bottomPanel.add(Box.createRigidArea(new Dimension(10, 0)));
+		
+		JLabel seedLabel = new JLabel("Seed:");
+		bottomPanel.add(seedLabel);
+		_seedText = new JTextField(5);
+		bottomPanel.add(_seedText);
+		bottomPanel.add(Box.createRigidArea(new Dimension(10, 0)));
+		
+		JButton button = new JButton("Setup");
 		button.setActionCommand("setup");
 		button.addActionListener(this);
-		buttonsPanel.add(button);
-		buttonsPanel.add(Box.createRigidArea(new Dimension(10, 0)));
-		button = new JButton("Start");
-		button.setActionCommand("start");
-		button.addActionListener(this);
-		buttonsPanel.add(button);
-		buttonsPanel.add(Box.createRigidArea(new Dimension(10, 0)));
-		button = new JButton("Stop");
-		button.setActionCommand("stop");
-		button.addActionListener(this);
-		buttonsPanel.add(button);
-		getContentPane().add(buttonsPanel, BorderLayout.SOUTH);
+		bottomPanel.add(button);
+		bottomPanel.add(Box.createRigidArea(new Dimension(10, 0)));
+		
+		_startStopButton = new JButton("Start");
+		_startStopButton.setActionCommand("start");
+		_startStopButton.addActionListener(this);
+		bottomPanel.add(_startStopButton);
+		getContentPane().add(bottomPanel, BorderLayout.SOUTH);
 		
 		pack();
-		addComponentListener(this);
 	}
 
 	@Override
@@ -101,44 +144,49 @@ public class GameUI extends JFrame implements ActionListener, ComponentListener 
 			onStart();
 		} else if ("stop".equals(e.getActionCommand())) {
 			onStop();
+		} else if ("gridSmall".equals(e.getActionCommand())) {
+			_gameBoard._gridSize = GameBoard.GRID_SMALL;
+			_gameBoard.repaint();
+		} else if ("gridMedium".equals(e.getActionCommand())) {
+			_gameBoard._gridSize = GameBoard.GRID_MEDIUM;
+			_gameBoard.repaint();
+		} else if ("gridLarge".equals(e.getActionCommand())) {
+			_gameBoard._gridSize = GameBoard.GRID_LARGE;
+			_gameBoard.repaint();
 		}
 	}
 
 	@Override
-	public void componentHidden(ComponentEvent arg0) {
-		// nothing
-	}
-
-	@Override
-	public void componentMoved(ComponentEvent arg0) {
-		// nothing
-	}
-
-	@Override
-	public void componentResized(ComponentEvent arg0) {
-		System.out.println(String.format("%1$d, %2$d", _gameBoard.getWidth(), _gameBoard.getHeight()));
-	}
-
-	@Override
-	public void componentShown(ComponentEvent arg0) {
-		// nothing
+	public void stateChanged(ChangeEvent e) {
+		JSpinner spinner = (JSpinner)(e.getSource());
+		_speed = MAX_SPEED + 1 - (int) spinner.getValue();
+		if (_gameThread != null)
+			_gameThread.setFramePeriod(_speed * 10);
 	}
 	
 	class GameBoard extends JPanel {
-		private static final long serialVersionUID = 1L;
+		private static final long serialVersionUID = 1L;	
 		
-		public static final int GRID_SIZE = 10;
+		private static final int GRID_SMALL = 10;
+		private static final int GRID_MEDIUM = 25;
+		private static final int GRID_LARGE = 50;
+		
+		private int _gridSize = GRID_SMALL;
 		
 		public GameBoard() {
 			setBackground(new Color(33, 33, 33));
 		}
 		
-		public int getGridWidth() {
-			return getWidth() / GRID_SIZE;
+		public int getGameWidth() {
+			return getWidth() / _gridSize;
 		}
 		
-		public int getGridHeight() {
-			return getHeight() / GRID_SIZE;
+		public int getGameHeight() {
+			return getHeight() / _gridSize;
+		}
+		
+		public void setGridSize(int gridSize) {
+			_gridSize = gridSize;
 		}
 		
 		@Override
@@ -149,23 +197,23 @@ public class GameUI extends JFrame implements ActionListener, ComponentListener 
 		@Override
 		protected void paintComponent(Graphics g) {
 			super.paintComponent(g);
-			int gridWidth = getWidth() / GRID_SIZE;
-			int gridHeight = getHeight() / GRID_SIZE;
-			int xOffset = (getWidth() - gridWidth * GRID_SIZE) / 2;
-			int yOffset = (getHeight() - gridHeight * GRID_SIZE) / 2;
+			int gridWidth = getWidth() / _gridSize;
+			int gridHeight = getHeight() / _gridSize;
+			int xOffset = (getWidth() - gridWidth * _gridSize) / 2;
+			int yOffset = (getHeight() - gridHeight * _gridSize) / 2;
 			g.setColor(new Color(128, 128, 128));
 			for (int x = 0; x <= gridWidth; x++) {
-				g.drawLine(xOffset + x * GRID_SIZE, yOffset, xOffset + x * GRID_SIZE, yOffset + gridHeight * GRID_SIZE);
+				g.drawLine(xOffset + x * _gridSize, yOffset, xOffset + x * _gridSize, yOffset + gridHeight * _gridSize);
 			}
 			for (int y = 0; y <= gridHeight; y++) {
-				g.drawLine(xOffset, yOffset + y * GRID_SIZE, xOffset + gridWidth * GRID_SIZE, yOffset + y * GRID_SIZE);
+				g.drawLine(xOffset, yOffset + y * _gridSize, xOffset + gridWidth * _gridSize, yOffset + y * _gridSize);
 			}
 			g.setColor(new Color(255, 153, 0));
 			if (_gameState != null) {
 				for (int x = 0; x < _gameState.getWidth(); x++) {
 					for (int y = 0; y < _gameState.getHeight(); y++) {
 						if (_gameState.isAlive(y, x))
-						g.fillRect(x * GRID_SIZE + 1, y * GRID_SIZE + 1, GRID_SIZE - 1, GRID_SIZE - 1);
+						g.fillRect(xOffset + x * _gridSize + 1, yOffset + y * _gridSize + 1, _gridSize - 1, _gridSize - 1);
 					}
 				}
 			}
